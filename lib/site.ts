1e98f8d56e4ab1e31579622d5dbed069456155c9
telluricaquarian <llewellyn@telluricaquarian.com>
2025-09-06 15:52:29 +1000

    Initial commit - Magic UI blog

---
+++ lib/site.ts
@@ -0,0 +1,8 @@
+export const siteConfig = {
+  name: "Magic UI Blog",
+  url: "https://blog-magicui.vercel.app",
+  description:
+    "Modern React and web development tutorials, insights, and best practices.",
+};
+
+export type SiteConfig = typeof siteConfig;
